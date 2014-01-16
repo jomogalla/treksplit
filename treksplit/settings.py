88f0f8dc431badd7a52a1e2ed1ed07ef8be35c36
Jason Domogalla <j@Jasons-MacBook-Pro.local>
2014-01-16 00:24:03 -0800

    i think it isg

--- treksplit/settings.py
+++ treksplit/settings.py
@@ -120,6 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/j/Dropbox/treksplit_mac/templates/"
+    # "/Users/j/Dropbox/treksplit_mac/templates/"
     # "C:/Users/Jason/Dropbox/pyWeather/templates"
+    os.path.join(PROJECT_DIR, "templates"),
 )
